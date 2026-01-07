8d39ad1139222a8ae24c8621f9a370d345b500cb
themanojking <kmanoj71737@gmail.com>
2026-01-07 22:44:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-red-500 font-bold'>Heritage - Temples</h1>
+    </div>
+  )
+}
+
+export default App
