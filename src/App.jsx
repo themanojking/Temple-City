662dc09ee0c8f4b3c8bc3740487bc262b78de5ad
candymani-08 <manirathnam874@gmail.com>
2026-01-07 23:04:38 +0530

    last once

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
       <h1 className='text-5xl text-red-500 font-bold'>Heritage - Temples city</h1>
       <h3>MiniProject for final year</h3>
       <h5>final demo4</h5>
+      <h3>last one</h3>
     </div>
   )
 }
