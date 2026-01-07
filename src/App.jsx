b993f17069258793572ddfe63f6d903a4246e14b
candymani-08 <manirathnam874@gmail.com>
2026-01-07 22:56:31 +0530

    demo1

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='text-5xl text-red-500 font-bold'>Heritage - Temples city</h1>
+      <h1 className='text-5xl text-red-500 font-bold'>Heritage - Temples city   </h1>
     </div>
   )
 }
