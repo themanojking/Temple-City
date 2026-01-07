902448c435fc02f9942cea4c6f5aec47b947e884
candymani-08 <manirathnam874@gmail.com>
2026-01-07 23:02:34 +0530

    demo4

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
     <div>
       <h1 className='text-5xl text-red-500 font-bold'>Heritage - Temples city</h1>
       <h3>MiniProject for final year</h3>
+      <h5>final demo4</h5>
     </div>
   )
 }
